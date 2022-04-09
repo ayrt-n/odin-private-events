6d4eb9cf136865c949e5ef9b0538e529511039cc
ayrton <ayrton.parkinson1@gmail.com>
2022-04-09 14:23:42 -0400

    Fix association HMT between User Event and UserEvents

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
-  has_many :events_users, foreign_key: 'attended_event_id'
-  has_many :attendees, through: :events_users
+  has_many :user_events, foreign_key: 'attended_event_id'
+  has_many :attendees, through: :user_events
 end
