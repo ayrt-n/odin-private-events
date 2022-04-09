4dbde3519cb17a60fa980e1d18ad2a51184f84a5
ayrton <ayrton.parkinson1@gmail.com>
2022-04-09 11:00:25 -0400

    Add many-to-many association between events and users attending

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
+  has_and_belongs_to_many :attendees,
+                          class_name: 'User',
+                          foreign_key: 'attended_event_id',
+                          association_foreign_key: 'attendee_id'
 end
