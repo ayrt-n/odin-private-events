246d62c0647f5778a8ec5b81340410cc44c4ee66
ayrton <ayrton.parkinson1@gmail.com>
2022-04-11 07:03:53 -0400

    Add class method to query past and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :user_events, foreign_key: 'attended_event_id'
   has_many :attendees, through: :user_events
+
+  def self.past
+    self.where('end_time < :current_time', { current_time: Time.now })
+  end
+
+  def self.upcoming
+    self.where('end_time >= :current_time', { current_time: Time.now })
+  end
 end
