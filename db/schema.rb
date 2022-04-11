5b33ad3c4afb920dafcbd6b6a9c820b8576c539a
ayrton <ayrton.parkinson1@gmail.com>
2022-04-11 07:35:41 -0400

    Add username and uniq index to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_11_110912) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_11_113300) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
@@ -42,8 +42,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_11_110912) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "user_events", "events", column: "attended_event_id"
