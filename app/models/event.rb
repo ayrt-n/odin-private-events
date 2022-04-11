0db164402b2ceab6948da49c0072f99aeac11498
ayrton <ayrton.parkinson1@gmail.com>
2022-04-11 07:07:04 -0400

    Refactor self.past and self.upcoming into scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,11 +3,6 @@ class Event < ApplicationRecord
   has_many :user_events, foreign_key: 'attended_event_id'
   has_many :attendees, through: :user_events
 
-  def self.past
-    self.where('end_time < :current_time', { current_time: Time.now })
-  end
-
-  def self.upcoming
-    self.where('end_time >= :current_time', { current_time: Time.now })
-  end
+  scope :past, -> { where('end_time < :current_time', { current_time: Time.now }) }
+  scope :upcoming, -> { where('end_time >= :current_time', { current_time: Time.now }) }
 end
