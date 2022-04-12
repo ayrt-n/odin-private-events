4bc05886c01a0f2766b126386b31823b8d13ec80
ayrton <ayrton.parkinson1@gmail.com>
2022-04-12 07:34:04 -0400

    Add presence validator to Event attributes

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,8 @@ class Event < ApplicationRecord
   has_many :user_events, foreign_key: 'attended_event_id'
   has_many :attendees, through: :user_events
 
+  validates :name, :description, :start_time, :end_time, presence: true
+
   scope :past, -> { where('end_time < :current_time', { current_time: Time.now }) }
   scope :upcoming, -> { where('end_time >= :current_time', { current_time: Time.now }) }
 end
