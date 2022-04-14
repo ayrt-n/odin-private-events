c78e89576d54314d6c3420b7359728498cf5ad29
ayrton <ayrton.parkinson1@gmail.com>
2022-04-14 08:04:51 -0400

    Add redirect_to event after attending/leaving

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -3,11 +3,15 @@ class UserEventsController < ApplicationController
 
   def update
     @event = Event.find(params[:id])
-    current_user.attended_events << @event
+    current_user.attended_events << @event unless current_user.attended_events.include?(@event)
+
+    redirect_to @event
   end
 
   def destroy
     @event = Event.find(params[:id])
     current_user.attended_events.destroy(@event)
+
+    redirect_to @event
   end
 end
