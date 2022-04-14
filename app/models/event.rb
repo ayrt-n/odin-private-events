030fbda85a9e69b937812e3243ab58b316562cd1
ayrton <ayrton.parkinson1@gmail.com>
2022-04-14 08:04:06 -0400

    Add validation for location and event time

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,10 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
-  has_many :user_events, foreign_key: 'attended_event_id'
+  has_many :user_events, foreign_key: 'attended_event_id', dependent: :destroy
   has_many :attendees, through: :user_events
 
-  validates :name, :description, :start_time, :end_time, presence: true
+  validates :name, :location, :description, :start_time, :end_time, presence: true
+  validates :start_time, comparison: { less_than: :end_time }
 
   scope :past, -> { where('end_time < :current_time', { current_time: Time.now }) }
   scope :upcoming, -> { where('end_time >= :current_time', { current_time: Time.now }) }
